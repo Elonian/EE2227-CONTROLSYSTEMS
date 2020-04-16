6ada186e6355096e2b659547c19ce884ccd1af18
varunsankarmoparthi <48233607+varunsankarmoparthi@users.noreply.github.com>
2020-04-16 15:23:20 +0530

    Create sys_response_plot.py

---
+++ codes/sys_response_plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+t = np.linspace(0, 30,1000)
+
+y = (8.00/73.00)*np.cos(2*t) +(152.00/73.00)*np.sin(2*t) + (-8.00/73.00)*np.exp(-3*t/4)
+
+plt.grid()
+plt.title("System response gm(t) vs t ")
+plt.xlabel("Time")
+plt.ylabel("Oscillating system response gm(t)")
+plt.plot(t,y)
+plt.show()
